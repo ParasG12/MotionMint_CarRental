bd09309eca9ed0fab79c381b7aeb1f1c9173d793
ParasG12 <parasseth16121999@gmail.com>
2024-08-26 16:04:05 +0530

    message

---
+++ MotionMintDotnet/CarVariantMVC/CarVariantMVC/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using CarVariantMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarVariantMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly CarVariantTestContext _context;
+
+        public AdminController(CarVariantTestContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult AdminDashboard()
+        {
+          
+          
+                // Fetch the counts for the dashboard boxes
+                var model = new AdminDashboardViewModel
+                {
+                    CompanyCount = _context.Companies.Count(),
+                    VariantCount = _context.Variants.Count(),
+                    CustomerCount = _context.Users.Count(),
+                    BookingCount = _context.Bookings.Count()
+                };
+
+                return View(model); // 
+            
+            
+        }
+    }
+}
